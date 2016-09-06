146c62c4288431dbc3875a3628817ee66d8e86fe
grantmx <support@grantmx.com>
2016-09-06 08:59:37 -0400

    rebuilding to see if it fixes in app browser link issue

--- scripts/source/app.js
+++ scripts/source/app.js
@@ -1,43 +1,82 @@
-
 var app = angular.module('app', [
-  'ngRoute', 'ngTouch', 'angular-carousel', 'ngSanitize'
+    'ngRoute', 'ngTouch', 'angular-carousel', 'ngSanitize'
 ]);
 
-app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-  $routeProvider
+app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+    $routeProvider
     // Home
-    .when("/", {templateUrl: "http://prazor.com/app/scripts/source/modules/genres/views/genres.html", controller: 'GenreController'})
-    // Pages
-    .when("/music/genres", {templateUrl: "http://prazor.com/app/scripts/source/modules/genres/views/genres.html", controller: 'GenreController'})
-	.when("/music/stations", {templateUrl: "http://prazor.com/app/scripts/source/modules/stations/views/stations.html", controller: 'StationController'})
-	.when("/music/audioplayer", {templateUrl: "http://prazor.com/app/scripts/source/modules/stations/views/station-player-view.html", controller: 'StationController'})
-	.when("/teaching", {templateUrl: "http://prazor.com/app/scripts/source/templates/categories.html", controller: 'TeachingCategoriesController'})
-	.when("/teaching/teaching", {templateUrl: "http://prazor.com/app/scripts/source/templates/subCategory.html", controller: 'TeachingController'})
-	.when("/teaching/playlist", {templateUrl: "http://prazor.com/app/scripts/source/templates/playlist.html", controller: 'PlaylistController'})
-	.when("/teaching/video", {templateUrl: "http://prazor.com/app/scripts/source/templates/video.html", controller: 'VideoController'})
-	.when("/talk", {templateUrl: "http://prazor.com/app/scripts/source/templates/categories.html", controller: 'TalkCategoriesController'})
-	.when("/talk/talk", {templateUrl: "http://prazor.com/app/scripts/source/templates/subCategory.html", controller: 'TalkController'})
-	.when("/talk/playlist", {templateUrl: "http://prazor.com/app/scripts/source/templates/playlist.html", controller: 'PlaylistController'})
-	.when("/talk/video", {templateUrl: "http://prazor.com/app/scripts/source/templates/video.html", controller: 'VideoController'})
-    // else 404
-	.when("/404", {templateUrl: "http://prazor.com/app/scripts/source/modules/pageError/views/404.html", controller: 'PageErrorController'})
-	.otherwise({redirectTo:'/404'});
+        .when("/", {
+            templateUrl: "http://prazor.com/app/scripts/source/modules/genres/views/genres.html",
+            controller: 'GenreController'
+        })
+        // Pages
+        .when("/music/genres", {
+            templateUrl: "http://prazor.com/app/scripts/source/modules/genres/views/genres.html",
+            controller: 'GenreController'
+        })
+        .when("/music/station/:stationId", {
+            templateUrl: "http://prazor.com/app/scripts/source/modules/stations/views/stations.html",
+            controller: 'StationController'
+        })
+        .when("/music/audioplayer", {
+            templateUrl: "http://prazor.com/app/scripts/source/modules/stations/views/station-player-view.html",
+            controller: 'StationController'
+        })
+        .when("/teaching", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/categories.html",
+            controller: 'TeachingCategoriesController'
+        })
+        .when("/teaching/teaching", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/subCategory.html",
+            controller: 'TeachingController'
+        })
+        .when("/teaching/playlist", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/playlist.html",
+            controller: 'PlaylistController'
+        })
+        .when("/teaching/video", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/video.html",
+            controller: 'VideoController'
+        })
+        .when("/talk", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/categories.html",
+            controller: 'TalkCategoriesController'
+        })
+        .when("/talk/talk", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/subCategory.html",
+            controller: 'TalkController'
+        })
+        .when("/talk/playlist", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/playlist.html",
+            controller: 'PlaylistController'
+        })
+        .when("/talk/video", {
+            templateUrl: "http://prazor.com/app/scripts/source/templates/video.html",
+            controller: 'VideoController'
+        })
+        // else 404
+        .when("/404", {
+            templateUrl: "http://prazor.com/app/scripts/source/modules/pageError/views/404.html",
+            controller: 'PageErrorController'
+        })
+        .otherwise({
+            redirectTo: '/404'
+        });
 }]);
 
 
 app.initialize = function() {
-	this.bindEvents();
+    this.bindEvents();
 };
-    
+
 app.bindEvents = function() {
-	document.addEventListener('deviceready', this.onDeviceReady, true);
+    document.addEventListener('deviceready', this.onDeviceReady, true);
 };
 
 app.onDeviceReady = function() {
-	angular.element(document).ready(function() {
-		angular.bootstrap(document);
-	});
+    angular.element(document).ready(function() {
+        angular.bootstrap(document);
+    });
 
-	window.analytics.startTrackerWithId('UA-73794751-1');
+    window.analytics.startTrackerWithId('UA-73794751-1');
 };
-
--- scripts/source/modules/genres/controller/GenreController.js
+++ scripts/source/modules/genres/controller/GenreController.js
@@ -6,14 +6,14 @@
 		var config = {
 			method : 'GET',
 			url : 'http://prazor.com/rest-dev/getGenres/'
-		}
+		};
 
 		dataService.getData(config).then(
 			function(response){
 				$scope.genres = response.data;
 			},function(error){
 				console.log(error);
-			});	
+			});
 
 		// $scope.genres = [{"title":"Premium","genre_image":"","entry_id":9},{"title":"Pop","genre_image":"","entry_id":2},{"title":"Rock","genre_image":"","entry_id":1},{"title":"Inspirational","genre_image":"","entry_id":4},{"title":"Urban","genre_image":"","entry_id":5},{"title":"Country","genre_image":"","entry_id":7},{"title":"World","genre_image":"","entry_id":8},{"title":"Blended","genre_image":"","entry_id":3}];
 
@@ -21,7 +21,7 @@
 			DataFactory.currentStation.genre = genre;
 			$scope.station = DataFactory.currentStation.station;
 			DataFactory.station = {};
-			$location.url('music/stations');
+			$location.url('music/station');
 		};
 	}]);
 })();
